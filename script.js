b70a69273f37baf086fd9b89557fd4d39dbae52c
AldJayR <aldwinjzs3@gmail.com>
2024-09-07 19:25:55 +0800

    Added display weather function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const cityInput = document.getElementById("city-input");
 const enterBtn = document.getElementById("enter-btn");
 const weatherCityForm = document.getElementById("weather-city-form");
 const API_KEY = "23c5ed2ad62bd6686b523648cd3b6700";
+const weatherDataContainer = document.querySelector(".weather-data-container");
 
 weatherCityForm.addEventListener("submit", e => {
     e.preventDefault();
@@ -20,9 +21,10 @@ const fetchData = async () => {
     try {
         const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`);
         const data = await res.json();
-        console.log(data);
+        displayWeatherData(data);
     } catch (err) {
         console.log(err);
+        weatherDataContainer.innerHTML = `City not found`;
     }
 };
 
@@ -30,7 +32,20 @@ const fetchData = async () => {
 
 const displayWeatherData = data => {
     const { weather, main: mainWeather, sys, name } = data;
-    const { main, description, icon } = weather;
+    const [{ main, description, icon }] = weather;
     const { temp, feels_like, temp_min, temp_max, humidity } = mainWeather;
+    const { country } = sys;
+
+    weatherDataContainer.innerHTML = `
+        <h2 class="city-name">${name}</h2>
+        <small>${country}</small>
+        <img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="Icon of ${description}" class="weather-icon" />
+        <h5 class="main-weather">${main}</h5>
+        <p class="temperature">${(temp - 273.15).toFixed(2)}°C</p>
+        <p>Min: ${(temp_min - 273.15).toFixed(2)}°C</p>
+        <p>Max: ${(temp_max - 273.15).toFixed(2)}°C</p>
+        <p>Feels like: ${(feels_like - 273.15).toFixed(2)}°C</p>
+
+    `
 }
 
