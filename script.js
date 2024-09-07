b7be6b492ee231afff7befb971ae098dd477d30c
AldJayR <aldwinjzs3@gmail.com>
2024-09-07 18:39:55 +0800

    Added input validation

--- script.js
+++ script.js
@@ -3,14 +3,34 @@ const enterBtn = document.getElementById("enter-btn");
 const weatherCityForm = document.getElementById("weather-city-form");
 const API_KEY = "23c5ed2ad62bd6686b523648cd3b6700";
 
+weatherCityForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const city = cityInput.value;
+    if (!city) {
+        alert("Invalid input.");
+        return false;
+    }
+
+    fetchData();
+});
+
+
 const fetchData = async () => {
     const city = cityInput.value;
     try {
         const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`);
         const data = await res.json();
+        console.log(data);
     } catch (err) {
         console.log(err);
     }
+};
+
+
+
+const displayWeatherData = data => {
+    const { weather, main: mainWeather, sys, name } = data;
+    const { main, description, icon } = weather;
+    const { temp, feels_like, temp_min, temp_max, humidity } = mainWeather;
 }
 
-enterBtn.onclick = () => console.log(cityInput.value);
